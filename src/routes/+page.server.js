ea2a54101da546dc29933b7283464be2fba75c0b
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-11-08 16:16:09 +0100

    Mostly working landing page with project cards

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,23 +1,15 @@
 import { client } from '$lib/graphql-client'
-import { gql } from 'graphql-request'
+import { authorsQuery, projectsQuery } from '$lib/graphql-queries'
  
 export async function load({ params }) {
-    const query = gql`
-          query GetProjects {
-            projects {
-              name
-              slug
-              description
-              demo
-              sourceCode
-              image {
-                url
-              }
-            }
-          }
-        `
-    const { projects } = await client.request(query)
+    const [authorReq, projectsReq] = await Promise.all([
+      client.request(authorsQuery),
+      client.request(projectsQuery),
+    ])
+    const { authors } = authorReq
+    const { projects } = projectsReq
     return {
         projects,
+        authors,
     }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,28 @@
     export let data
 </script>
 
-<h1>Recent Projects by Me</h1>
+<svelte:head>
+  <title>The Endurance Engineer</title>
+</svelte:head>
 
-<div>
+<h1 class="font-bold text-center mb-20 text-5xl">
+  Welcome to my Portfolio
+</h1>
+
+{#each data.authors as { name, intro, picture: { url } }}
+  <div class="flex mb-40 items-end">
+    <div class="mr-6">
+      <h2 class="text-3xl mb-4 font-bold tracking-wider">{name}</h2>
+      <p class="text-xl mb-4">{intro}</p>
+    </div>
+
+    <img class="mask mask-squircle h-48" src={url} alt={name} />
+  </div>
+{/each}
+
+<div
+  class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52"
+>
   {#each data.projects as { name, slug, description, image }}
     <ProjectCard {name} {description} url={image[0].url} {slug} />
   {/each}
