971392430a62c242a8246d03f6247386d62ad790
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-11-08 15:50:41 +0100

    add projectCard component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
  <script>
+    import ProjectCard from "../lib/components/project-card.svelte";
     export let data
 </script>
 
-<pre>{JSON.stringify(data.projects, null, 2)}</pre>
+<h1>Recent Projects by Me</h1>
+
+<div>
+  {#each data.projects as { name, slug, description, image }}
+    <ProjectCard {name} {description} url={image[0].url} {slug} />
+  {/each}
+</div>
