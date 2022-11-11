a2950c79fbc9fdfd1b17b0d7dd27881499d4101d
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-11-11 17:08:52 +0100

    Added Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Footer from '$lib/components/footer.svelte'
+	import Navbar from '$lib/components/navbar.svelte'
 	import '../app.postcss';
 </script>
 
+<Navbar />
 <main class="container max-w-3xl mx-auto px-4 mb-20">
-	<slot />
+  <slot />
 </main>
 <Footer />
